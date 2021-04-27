f0565e42c7a7ad598cb30bdcb83561bca7d352b0
Lucas Norberto <lucasnorberto@gmail.com>
2021-04-27 18:11:43 -0300

    update menu

--- tojob-v2-angular/src/app/app-routing.module.ts
+++ tojob-v2-angular/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './controller/home/home.component';
 import { MeuPainelComponent } from './controller/meu-painel/meu-painel.component';
 import { SairComponent } from './controller/sair/sair.component';
+import {VagasComponent} from './controller/vagas/vagas.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'meu-painel', component: MeuPainelComponent },
-  { path: 'sair', component: SairComponent }
+  { path: 'sair', component: SairComponent },
+  { path: 'vagas', component: VagasComponent },
+
 ];
 
 @NgModule({
