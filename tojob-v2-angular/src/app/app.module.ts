0054ddb09b856fa0e8b819b0c5f8ed4bb31c8f72
Lucas Norberto <77136264+lucasnorberto@users.noreply.github.com>
2021-05-09 01:39:05 -0300

    pagina home ajustes

--- tojob-v2-angular/src/app/app.module.ts
+++ tojob-v2-angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { }
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
